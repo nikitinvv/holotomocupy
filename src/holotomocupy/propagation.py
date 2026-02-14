d3087817e8827403d7b83d04575adcb3cabd5488
Viktor Nikitin <vnikitin@anl.gov>
2026-02-14 00:15:20 -0600

    optimize hessian

--- src/holotomocupy/propagation.py
+++ src/holotomocupy/propagation.py
@@ -13,11 +13,11 @@ class Propagation:
         fy = cp.fft.fftfreq(2*nz, d=voxelsize).astype("float32")
         [fx, fy] = cp.meshgrid(fx, fy)
         self.fker = cp.empty([ndist, 2*nz, 2*n], dtype="complex64")
-        self.ifker = cp.empty([ndist, 2*nz, 2*n], dtype="complex64")
+        # self.ifker = cp.empty([ndist, 2*nz, 2*n], dtype="complex64")
         for j in range(ndist):
             # propagation sample-detector
             self.fker[j] = cp.exp(-1j * cp.pi * wavelength * distance[j] * (fx**2 + fy**2))
-            self.ifker[j] = cp.exp(1j * cp.pi * wavelength * distance[j] * (fx**2 + fy**2))
+            # self.ifker[j] = cp.exp(1j * cp.pi * wavelength * distance[j] * (fx**2 + fy**2))
 
         self.n = n
         self.nz = nz
@@ -26,6 +26,7 @@ class Propagation:
         """Fwd data padding"""
         [ntheta, nz, n] = f.shape
         fpad = cp.zeros([ntheta, 2 * nz, 2 * n], dtype="complex64")
+        # f=cp.ascontiguousarray(f)
         pad_kernel(
             (math.ceil(2 * n / 32), math.ceil(2 * nz / 32), ntheta),
             (32, 32, 1),
@@ -39,6 +40,7 @@ class Propagation:
         n //= 2
         nz //=2
         f = cp.zeros([ntheta, nz, n], dtype="complex64")
+        # fpad=cp.ascontiguousarray(fpad)
         pad_kernel(
             (math.ceil(2 * n / 32), math.ceil(2 * nz / 32), ntheta),
             (32, 32, 1),
@@ -53,18 +55,17 @@ class Propagation:
             flg = 1
         else:
             flg = 0
-        big_psi = cp.empty([len(psi), self.nz, self.n], dtype="complex64")
-
-        ff = self._fwd_pad(psi)
+        
+        ff = psi.copy()
+        ff = self._fwd_pad(ff)
         ff = cp.fft.ifft2(cp.fft.fft2(ff) * self.fker[j])
         ff = ff[:, self.nz // 2 : -self.nz // 2, self.n // 2 : -self.n // 2]
-        big_psi[:] = ff
-
+        
         # big_psi[:] = cp.fft.ifft2(cp.fft.fft2(psi) * self.fker[j])
         if flg == 1:
-            big_psi = big_psi[0]
+            ff = ff[0]
 
-        return big_psi
+        return ff
 
     def DT(self, big_psi, j):
         """Adjoint propagator"""
@@ -73,15 +74,14 @@ class Propagation:
             flg = 1
         else:
             flg = 0
-        psi = cp.empty([len(big_psi), self.nz, self.n], dtype="complex64")
-
+        
         # pad to the probe size
-        ff = big_psi
+        ff = big_psi.copy()
         ff = cp.pad(ff, ((0, 0), (self.nz // 2, self.nz // 2), (self.n // 2, self.n // 2)))
-        ff = cp.fft.ifft2(cp.fft.fft2(ff) * self.ifker[j])
-        psi[:] = self._adj_pad(ff)
+        ff = cp.fft.ifft2(cp.fft.fft2(ff) / self.fker[j])
+        ff = self._adj_pad(ff)
 
-        # psi[:] = cp.fft.ifft2(cp.fft.fft2(big_psi) * 1 / self.fker[j])
+        # ff[:] = cp.fft.ifft2(cp.fft.fft2(big_psi) * 1 / self.fker[j])
         if flg == 1:
-            psi = psi[0]
-        return psi
+            ff = ff[0]
+        return ff

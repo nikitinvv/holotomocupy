0276c641faf50abd0e85cc1081713830bfeae87b
Viktor <vnikitin@anl.gov>
2026-06-28 16:49:30 +0000

    add Y350a_new

---
+++ experimental/Y350a_dist1234_new/polaris_run.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#PBS -A 14347
+#PBS -l select=24:system=polaris
+#PBS -l place=scatter
+#PBS -l filesystems=home:grand:eagle
+#PBS -l walltime=3:00:00
+#PBS -q prod
+#PBS -N holotomo
+#PBS -j oe
+
+# --- user configuration ---
+CONFIG=config_step6_bin2.conf
+SCRIPT=step6.py
+# --------------------------
+
+NNODES=$(wc -l < $PBS_NODEFILE)
+NRANKS=4
+NTHREADS=4
+NDEPTH=8
+export NTOTRANKS=$(( NNODES * NRANKS ))
+
+SCRIPT_DIR=/home/vvnikitin/holotomocupy/experimental/Y350a_dist1234_new #"$(pwd)"
+echo $SCRIPT_DIR
+rec_dir="$(dirname "${SCRIPT_DIR}")"
+
+# snapshot only py and conf files into a dated folder inside rec_dir
+scripts_dir="${rec_dir}/scripts$(date +%Y-%m-%d_%H-%M-%S)"
+mkdir -p "${scripts_dir}"
+cp "${SCRIPT_DIR}"/*.py   "${scripts_dir}/" 2>/dev/null || true
+cp "${SCRIPT_DIR}"/*.conf "${scripts_dir}/" 2>/dev/null || true
+
+cd "${rec_dir}"
+exec > >(tee "${scripts_dir}/slurm-${PBS_JOBID}.out" "${SCRIPT_DIR}/slurm-${PBS_JOBID}.out") 2>&1
+
+echo "Sample dir:  ${SCRIPT_DIR}"
+echo "Rec dir:     ${rec_dir}"
+echo "Snapshot:    ${scripts_dir}"
+echo "Jobid: $PBS_JOBID"
+echo "Running on host: $(hostname)"
+echo "Running on nodes: $(cat $PBS_NODEFILE)"
+echo "NUM_OF_NODES=${NNODES}  TOTAL_NUM_RANKS=${NTOTRANKS}  RANKS_PER_NODE=${NRANKS}"
+
+module use /soft/modulefiles;  module load conda; conda activate base
+CONDA_NAME=$(echo ${CONDA_PREFIX} | tr '\/' '\t' | sed -E 's/mconda3|\/base//g' | awk '{print $NF}')
+VENV_DIR="/home/vvnikitin/venvs/${CONDA_NAME}"
+source "${VENV_DIR}/bin/activate"
+
+export MATHDX_ROOT=/eagle/APS_IRI/vnikitin/nvidia/nvidia-mathdx-25.12.1-cuda12/nvidia/mathdx/25.12/
+# mpiexec -n ${NTOTRANKS} --ppn ${NRANKS} --depth=${NDEPTH} --cpu-bind depth --env OMP_NUM_THREADS=${NTHREADS} "${SCRIPT_DIR}/set_affinity_gpu_polaris.sh" python "${SCRIPT_DIR}/steps15.py" "${SCRIPT_DIR}/config_steps15.conf"
+mpiexec -n ${NTOTRANKS} --ppn ${NRANKS} --depth=${NDEPTH} --cpu-bind depth --env OMP_NUM_THREADS=${NTHREADS} "${SCRIPT_DIR}/set_affinity_gpu_polaris.sh" python "${SCRIPT_DIR}/step6.py" "${SCRIPT_DIR}/config_step6_bin2_cubic.conf"
+mpiexec -n ${NTOTRANKS} --ppn ${NRANKS} --depth=${NDEPTH} --cpu-bind depth --env OMP_NUM_THREADS=${NTHREADS} "${SCRIPT_DIR}/set_affinity_gpu_polaris.sh" python "${SCRIPT_DIR}/step6.py" "${SCRIPT_DIR}/config_step6_bin1_cubic.conf"
+mpiexec -n ${NTOTRANKS} --ppn ${NRANKS} --depth=${NDEPTH} --cpu-bind depth --env OMP_NUM_THREADS=${NTHREADS} "${SCRIPT_DIR}/set_affinity_gpu_polaris.sh" python "${SCRIPT_DIR}/step6.py" "${SCRIPT_DIR}/config_step6_bin0_cubic.conf"
